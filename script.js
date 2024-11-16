ceecd85623d8757edd607f33b8963b879273389b
Павел <zalevskiipavel112@gmail.com>
2024-11-16 20:29:35 +0200

    adaptation from 1400px to 768px + burger menu

--- script.js
+++ script.js
@@ -1,44 +1,82 @@
-let currentSlide = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    //burger
+    document.querySelector('.menu_button').addEventListener('click', () => {
+        let menu = document.querySelector('.hidden_nav');
+        let container = document.querySelector('.grid_container');
+        let bars = document.querySelectorAll('.bar');
+        let arrayClasses = ["first_bar",  "third_bar", "second_bar"], i = 0;
 
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
+        menu.classList.toggle('open');
+        container.classList.toggle('displ');
 
-document.querySelector('.next').addEventListener('click', () => {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    updateSlide();
+        bars.forEach(e => {
+            e.classList.toggle(arrayClasses[i]);
+            i++;
+        });
+        i = 0;
+    })
+    function checkWindowSize() {
+        const width = window.innerWidth;
+        
+        if (width > 992) {
+            document.querySelector('.hidden_nav').classList.remove('open');
+            document.querySelector('.grid_container').classList.remove('displ');
+        }
+    }
+
+    window.addEventListener('resize', checkWindowSize);
+    
+    checkWindowSize();
+
+    //slider events
+    let currentSlide = 0;
+
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    document.querySelector('.next').addEventListener('click', () => {
+        currentSlide = (currentSlide + 1) % totalSlides;
+        updateSlide();
+    });
+
+    document.querySelector('.prev').addEventListener('click', () => {
+        currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+        updateSlide();
+    });
+
+    function updateSlide() {
+        const slidesContainer = document.querySelector('.events-slides');
+        slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+    }
+
+    //slider products
+    let currentProductSlide = 0;
+
+    const productSlides = document.querySelectorAll('.product_slide');
+    const totalProductSlides = productSlides.length;
+
+    const slideWidth = document.querySelector('.product_slide');
+
+    //console.log(slideWidth.offsetWidth)
+
+    document.querySelector('.product_next').addEventListener('click', () => {
+        currentProductSlide = (currentProductSlide + 1) % totalProductSlides;
+        updateProductSlide();
+    });
+
+    document.querySelector('.product_prev').addEventListener('click', () => {
+        currentProductSlide = (currentProductSlide - 1 + totalProductSlides) % totalProductSlides;
+        updateProductSlide();
+    });
+
+    function updateProductSlide() {
+        const slidesContainer = document.querySelector('.products_slides');
+        slidesContainer.style.transform = `translateX(-${currentProductSlide * (slideWidth.offsetWidth + 30)}px)`;
+        //console.log(slideWidth.offsetWidth)
+    }
 });
 
-document.querySelector('.prev').addEventListener('click', () => {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    updateSlide();
-});
-
-function updateSlide() {
-    const slidesContainer = document.querySelector('.events-slides');
-    slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
-}
-
-let currentProductSlide = 0;
-
-const productSlides = document.querySelectorAll('.product_slide');
-const totalProductSlides = productSlides.length;
-
-document.querySelector('.product_next').addEventListener('click', () => {
-    currentProductSlide = (currentProductSlide + 1) % totalProductSlides;
-    updateProductSlide();
-});
-
-document.querySelector('.product_prev').addEventListener('click', () => {
-    currentProductSlide = (currentProductSlide - 1 + totalProductSlides) % totalProductSlides;
-    updateProductSlide();
-});
-
-function updateProductSlide() {
-    const slidesContainer = document.querySelector('.products_slides');
-    slidesContainer.style.transform = `translateX(-${currentProductSlide * 480}px)`;
-}
-
-/* <div class="slider">
+/* 480<div class="slider">
         <div class="slides">
             <div class="slide">Слайд 1</div>
             <div class="slide">Слайд 2</div>
